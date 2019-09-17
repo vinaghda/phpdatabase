6a3ffd89052351970bea80e4c98dfe7c89e8f03d
vinaghda <vinaghda@gmail.com>
2019-09-17 10:30:55 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+
+<?php
+echo "Welcome to OpenShift Online Developer Preview";
+echo "<br>To test the database, hit the dbtest.php URL";
+
+echo "<br><br>This assumes that you have the correct env variables set";
+echo "<BR><BR>The environment variables required are databaseuser, databasepassword, and databasenmae.";
